b821777e1dae22fdd008e13a42511196fe231517
Stuart Fleisher <stufleisher@gmail.com>
2023-07-25 19:55:37 -0400

    Working on selector

---
+++ outer_space.sql
@@ -0,0 +1,75 @@
+CREATE TABLE stars(
+    name VARCHAR(100) PRIMARY KEY,
+    temp_kelvin INT NOT NULL);
+
+INSERT INTO stars(name, temp_kelvin)
+    VALUES
+    ('The Sun', 5800),
+    ('Proxima Centauri', 3042),
+    ('Gliese 876', 3192);
+
+
+CREATE TABLE planets(
+    name VARCHAR(100) PRIMARY KEY,
+    orbital_period FLOAT NOT NULL,
+    star VARCHAR(100) REFERENCES stars);
+
+INSERT INTO planets
+    (name, orbital_period, star)
+    VALUES
+    ('Earth', 1.00, 'The Sun'),
+    ('Mars',1.882, 'The Sun'),
+    ('Venus',0.62, 'The Sun'),
+    ('Proxima Centauri b', 0.03, 'Proxima Centauri'),
+    ('Gliese 876 b', 0.236, 'Gliese 876');
+
+
+CREATE TABLE moons(
+    name VARCHAR(100) PRIMARY KEY,
+    planet VARCHAR(100) REFERENCES planets);
+
+INSERT INTO moons(name, planet)
+    VALUES
+    ('The Moon', 'Earth'),
+    ('Phobos', 'Mars'),
+    ('Deimos','Mars');
+
+SELECT name, star, COUNT(moon.name) as moon_count
+    FROM planets LEFT JOIN moons
+        ON planets.name=moons.planet;
+
+
+
+
+            SELECT COUNT(*)
+                FROM moons
+                GROUP BY planet
+            ON planets.name=moons.planet;
+
+
+
+
+STARS
+*name > varchar > Primary key
+temperature > int > not null
+
+
+stars_planets (has many)
+id > SERIAL > Primary key
+star_name > foreign key
+planet_name > foreign key
+
+PLANETS
+*name > primary key
+orbital_period > float > not null
+star > references
+
+
+planets_moons (has many)
+id SERIAL > Primary key
+planet_name > foreign key
+moon_name > foreign key
+
+
+MOONS
+*name > Primary Key
